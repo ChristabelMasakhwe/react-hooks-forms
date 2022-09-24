8052c88c02ca62d50c7b61b726b4f2823354e3fd
Christabel <christabel.masakhwe@student.moringaschool.com>
2022-09-24 13:24:22 +0300

    Completed assignment

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,11 +3,44 @@ import React, { useState } from "react";
 function Form() {
   const [firstName, setFirstName] = useState("John");
   const [lastName, setLastName] = useState("Henry");
+  const [newsletter, setNewsletter] = useState(false);
+  const [number, setNumber] = useState(0);
+  const [isInvalidNumber, setIsInvalidNumber] = useState(null);
 
+  function handleNumberChange(event) {
+    const newNumber = parseInt(event.target.value);
+    if (newNumber >= 0 && newNumber <= 5) {
+      setNumber(newNumber);
+      setIsInvalidNumber(null);
+    } else {
+      setIsInvalidNumber(`${newNumber} is not a valid number!`);
+    }
+  }
+
+
+
+  function handleFirstNameChange(event) {
+setFirstName(event.target.value)
+  }
+  function handleLastNameChange(event) {
+    setLastName(event.target.value)
+  }
+  function handleNewsletterChange(event) {
+    setNewsletter(event.target.checked)
+  }
   return (
     <form>
-      <input type="text" value={firstName} />
-      <input type="text" value={lastName} />
+      <input onChange={handleFirstNameChange} type="text" value={firstName} />
+      <input onChange={handleLastNameChange} type="text" value={lastName} />
+      <label htmlFor="newsletter">Subscribe to our Newsletter?</label>
+      <input
+        type="checkbox"
+        id="newsletter"
+        onChange={handleNewsletterChange}
+        checked={newsletter}
+      />
+      <input type="number" value={number} onChange={handleNumberChange} />
+      {isInvalidNumber ? <span style={{ color: "red" }}>{isInvalidNumber}</span> : null}
       <button type="submit">Submit</button>
     </form>
   );
